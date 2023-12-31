f1947f68c2dbdf6eab4fa29100d95a1613d55978
lupuilie <lupuiliealexandru@gmail.com>
2023-12-31 02:15:59 +0200

    update create board dialog

--- packages/web/app/board/@dialog/add/page.tsx
+++ packages/web/app/board/@dialog/add/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogTitle,
-  DialogHeader,
-  DialogContent,
-  DialogDescription,
-} from '@/components/ui/dialog';
+import { useState } from 'react';
+import { X } from 'lucide-react';
 import { useRouter } from 'next/navigation';
+
+import { Dialog, DialogTitle, DialogHeader, DialogContent } from '@/components/ui/dialog';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 export default function Page() {
   const router = useRouter();
+  const [columnInputNames, setColumnInputNames] = useState<string[]>(['Todo', 'Doing', 'Done']);
 
   return (
     <Dialog
@@ -24,17 +24,73 @@ export default function Page() {
     >
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Add New Board</DialogTitle>
+          <DialogTitle className="text-heading-l">Add New Board</DialogTitle>
         </DialogHeader>
-        <div>
-          <Button variant="secondary" className="w-full">
-            + Add New Column
-          </Button>
-        </div>
-        <div>
-          <Button className="w-full">Create New Board</Button>
+        <div className="grid gap-4 pt-6">
+          <div className="grid w-full items-center gap-2">
+            <Label htmlFor="board-name">Board Name</Label>
+            <Input id="board-name" type="text" placeholder="e.g. Web Design" />
+          </div>
+          <div className="grid gap-2">
+            <Label>Board Columns</Label>
+            <div className="grid gap-3 mt-2">
+              {columnInputNames.map((value, index) => (
+                <div key={index} className="flex gap-4">
+                  <Input
+                    id="board-name"
+                    type="text"
+                    placeholder="e.g. Todo"
+                    value={value}
+                    onChange={(event) => updateColumnName(index, event.target.value)}
+                  />
+                  <Button
+                    variant="ghost"
+                    size="icon"
+                    className="p-0"
+                    onClick={() => removeColumn(index)}
+                  >
+                    <X size={20} />
+                  </Button>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="pt-1 grid gap-6">
+            <div>
+              <Button variant="secondary" className="w-full" onClick={addColumn}>
+                + Add New Column
+              </Button>
+            </div>
+            <div>
+              <Button className="w-full" onClick={createBoard}>
+                Create New Board
+              </Button>
+            </div>
+          </div>
         </div>
       </DialogContent>
     </Dialog>
   );
+
+  function addColumn() {
+    setColumnInputNames((columnInputNames) => [...columnInputNames, '']);
+  }
+
+  function removeColumn(index: number) {
+    setColumnInputNames((columnInputNames) => {
+      if (columnInputNames.length === 1) return [''];
+
+      return columnInputNames.filter((_, i) => i !== index);
+    });
+  }
+
+  function updateColumnName(index: number, value: string) {
+    setColumnInputNames((columnInputNames) =>
+      columnInputNames.map((name, i) => (i === index ? value : name)),
+    );
+  }
+
+  function createBoard() {
+    console.log('create board ', columnInputNames);
+  }
 }
