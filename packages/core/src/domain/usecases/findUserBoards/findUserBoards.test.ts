36515c7819cc4fa45baca89b00e4c5d067d9f9c1
lupuilie <lupuiliealexandru@gmail.com>
2024-01-04 22:12:53 +0200

    added findUserBoards usecase

--- packages/core/src/domain/usecases/findUserBoards/findUserBoards.test.ts
+++ packages/core/src/domain/usecases/findUserBoards/findUserBoards.test.ts
@@ -1,12 +1,25 @@
-import { describe, expect, it, vi, Mocked, afterEach } from 'vitest';
-
-import { BoardRepository } from '@kanban-app/core/domain/repositories';
+import { vi, describe, it, expect, Mocked, afterEach } from 'vitest';
 
 import { findUserBoards } from './findUserBoards';
+import { NotFoundException } from '@kanban-app/core/domain/types';
+import { UserBuilder } from '@kanban-app/core/infrastructure/shared/UserBuilder';
+import { BoardAccessRepository, BoardRepository, UserRepository } from '@kanban-app/core/domain/repositories';
+import { BoardBuilder } from '@kanban-app/core/infrastructure/shared/BoardBuilder';
+import { BoardAccessBuilder } from '@kanban-app/core/infrastructure/shared/BoardAccessBuilder';
 
 describe('findUserBoards', () => {
-  const mockBoardRepository: Mocked<BoardRepository> = {
+  const user = UserBuilder.build();
+  const userId = user.id;
+
+  const mockUserRepository: Mocked<UserRepository> = {
     findById: vi.fn(),
+  };
+  const mockBoardRepository: Mocked<BoardRepository> = {
+    findBoardById: vi.fn(),
+    findBoardsByIds: vi.fn(),
+    findBoardsByUserId: vi.fn(),
+  };
+  const mockBoardAccessRepository: Mocked<BoardAccessRepository> = {
     findByUserId: vi.fn(),
   };
 
@@ -14,17 +27,45 @@ describe('findUserBoards', () => {
     vi.resetAllMocks();
   });
 
-  const runningUsecase = findUserBoards({ boardRepository: mockBoardRepository });
+  const usecase = findUserBoards({
+    userRepository: mockUserRepository,
+    boardRepository: mockBoardRepository,
+    boardAccessRepository: mockBoardAccessRepository,
+  });
 
-  describe("given the user doesn't have any boards", () => {
-    const userId = 'user-id';
+  const runningTheUsecase = () => usecase({ userId });
 
+  describe('given the user does not exist', () => {
+    it('should throw NotFoundException', async () => {
+      mockUserRepository.findById.mockResolvedValue(null);
+
+      await expect(runningTheUsecase()).rejects.toThrowError(NotFoundException);
+    });
+  });
+
+  describe('given the user does not have access to any board', () => {
     it('should return an empty array', async () => {
-      mockBoardRepository.findByUserId.mockResolvedValueOnce([]);
+      mockUserRepository.findById.mockResolvedValue(user);
+      mockBoardAccessRepository.findByUserId.mockResolvedValue([]);
 
-      const boards = await runningUsecase({ userId });
+      const result = await runningTheUsecase();
 
-      expect(boards).toEqual([]);
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe('given the user has access to one board', () => {
+    it('should return an array with one board', async () => {
+      const board = BoardBuilder.build();
+      const boardAccess = BoardAccessBuilder.build({ userId, boardId: board.id });
+
+      mockUserRepository.findById.mockResolvedValue(user);
+      mockBoardAccessRepository.findByUserId.mockResolvedValue([boardAccess]);
+      mockBoardRepository.findBoardsByIds.mockResolvedValue([board]);
+
+      const result = await runningTheUsecase();
+
+      expect(result).toEqual([board]);
     });
   });
 });
--- packages/core/src/domain/usecases/findUserBoards/findUserBoards.ts
+++ packages/core/src/domain/usecases/findUserBoards/findUserBoards.ts
@@ -1,9 +1,11 @@
-import { BoardRepository } from '@kanban-app/core/domain/repositories/BoardRepository';
 import { Board } from '@kanban-app/core/domain/entities/Board';
-import { UseCaseConstructor } from '@kanban-app/core/domain/types/Usecase';
+import { UseCaseConstructor, NotFoundException } from '@kanban-app/core/domain/types';
+import { BoardRepository, UserRepository, BoardAccessRepository } from '@kanban-app/core/domain/repositories';
 
 type Params = {
   boardRepository: BoardRepository;
+  boardAccessRepository: BoardAccessRepository;
+  userRepository: UserRepository;
 };
 
 type Request = {
@@ -11,13 +13,38 @@ type Request = {
 };
 
 export const findUserBoards: UseCaseConstructor<Params, Request, Board[]> = (params) => {
-  const { boardRepository } = params;
+  const { boardRepository, boardAccessRepository, userRepository } = params;
 
   return async (request) => {
     const { userId } = request;
 
-    const boards = await boardRepository.findByUserId(userId);
+    await validateUser(userId);
+
+    const boardAccesses = await getUserBoardAccesses(userId);
+
+    if (boardAccesses.length === 0) {
+      return [];
+    }
+
+    const boardsIds = boardAccesses.map((boardAccess) => boardAccess.boardId);
+    const boards = await getBoards(boardsIds);
 
     return boards;
   };
+
+  async function validateUser(userId: string) {
+    const user = await userRepository.findById(userId);
+
+    if (!user) {
+      throw new NotFoundException(`User with id <${userId}> does not exist`);
+    }
+  }
+
+  async function getUserBoardAccesses(userId: string) {
+    return await boardAccessRepository.findByUserId(userId);
+  }
+
+  async function getBoards(boardsIds: string[]) {
+    return await boardRepository.findBoardsByIds(boardsIds);
+  }
 };
